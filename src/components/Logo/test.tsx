8546f90ff6947ef788f5feaa87eb2f25b1eb1e53
Gustavo Cotta <gmcotta34@gmail.com>
2021-09-26 13:09:26 -0300

    test: size prop for Logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -4,17 +4,31 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render a white label', () => {
+  it('should render the logo with white label', () => {
     renderWithTheme(<Logo />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#FAFAFA'
     })
   })
 
-  it('should render a black label when color prop is passed', () => {
+  it('should render the logo with black label', () => {
     renderWithTheme(<Logo color="black" />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#030517'
+    })
+  })
+
+  it('should render the logo with normal size', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render the logo with large size', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
     })
   })
 })
