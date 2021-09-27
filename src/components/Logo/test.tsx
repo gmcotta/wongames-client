02f5a47778a82c5345054dffa0f5e0553d8e50ee
Gustavo Cotta <gmcotta34@gmail.com>
2021-09-27 07:36:57 -0300

    test: add test for hideLabel

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,6 +1,7 @@
 import { screen } from '@testing-library/react'
-import { renderWithTheme } from 'utils/tests/helpers'
+import 'jest-styled-components'
 
+import { renderWithTheme } from 'utils/tests/helpers'
 import Logo from '.'
 
 describe('<Logo />', () => {
@@ -30,5 +31,16 @@ describe('<Logo />', () => {
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '20rem'
     })
+  })
+
+  it('should render the logo without label', () => {
+    renderWithTheme(<Logo size="large" hideLabel />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
   })
 })
