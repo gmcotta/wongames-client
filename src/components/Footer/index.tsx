18059b2bd6609a4c048906d2ff42bd39d12ad0ba
Gustavo Cotta <gmcotta34@gmail.com>
2021-09-30 09:30:12 -0300

    feat: create footer styles and tests

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,76 @@
+import Link from 'next/link'
+
+import Heading from 'components/Heading'
+import Logo from 'components/Logo'
 import * as S from './styles'
 
 const Footer = () => (
   <S.Wrapper>
-    <h1>Footer</h1>
+    <Logo color="black" />
+    <S.Content>
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          CONTATO
+        </Heading>
+        <a href="maito:sac@wongames.com">sac@wongames.com</a>
+        <span>+11 94038-0290</span>
+      </S.Column>
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          NOS ACOMPANHE
+        </Heading>
+        <nav aria-labelledby="Social Media">
+          <a
+            href="https://www.instagram.com/won-games"
+            target="_blank"
+            rel="noopener, noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://www.twitter.com/won-games"
+            target="_blank"
+            rel="noopener, noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            href="https://www.youtube.com/won-games"
+            target="_blank"
+            rel="noopener, noreferrer"
+          >
+            YouTube
+          </a>
+          <a
+            href="https://www.facebook.com/won-games"
+            target="_blank"
+            rel="noopener, noreferrer"
+          >
+            Facebook
+          </a>
+        </nav>
+      </S.Column>
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          LINKS
+        </Heading>
+        <nav aria-labelledby="Footer Resources">
+          <Link href="/">Loja</Link>
+          <Link href="/explore">Explorar</Link>
+          <Link href="/search">Buscar</Link>
+          <Link href="/faq">FAQ</Link>
+        </nav>
+      </S.Column>
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          LOCALIZAÇÃO
+        </Heading>
+        <span>Rua 7 de Maio</span>
+        <span>527 - 89020330</span>
+        <span>Rio de Janeiro, Brasil</span>
+      </S.Column>
+    </S.Content>
+    <S.Copyright>Won Games 2020 © Todos os Direitos Reservados</S.Copyright>
   </S.Wrapper>
 )
 
--- src/components/Footer/test.tsx
+++ src/components/Footer/test.tsx
@@ -1,13 +1,21 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Footer from '.'
 
 describe('<Footer />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Footer />)
-
-    expect(screen.getByRole('heading', { name: /Footer/i })).toBeInTheDocument()
-
+  it('should render the footer with 4 columns', () => {
+    const { container } = renderWithTheme(<Footer />)
+    expect(
+      screen.getByRole('heading', { name: /contato/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /nos acompanhe/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /links/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /localização/i })
+    ).toBeInTheDocument()
     expect(container.firstChild).toMatchSnapshot()
   })
 })
