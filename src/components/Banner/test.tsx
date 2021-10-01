69868b499426d59186b17d617f7597cf8cb0f5ed
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-01 08:32:52 -0300

    feat: finish Banner styles and generate snapshot

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -3,27 +3,23 @@ import { renderWithTheme } from 'utils/tests/helpers'
 
 import Banner from '.'
 
+const bannerProps = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
 describe('<Banner />', () => {
   it('should render the banner', () => {
-    const img = 'https://source.unsplash.com/user/willianjusten/1042x580'
-    const title = 'Defy death'
-    const subtitle = '<p>Play the new <strong>CrashLands</strong> season'
-    const buttonLabel = 'Buy now'
-    const buttonLink = '/games/defy-death'
-    renderWithTheme(
-      <Banner
-        img={img}
-        title={title}
-        subtitle={subtitle}
-        buttonLabel={buttonLabel}
-        buttonLink={buttonLink}
-      />
-    )
+    const { container } = renderWithTheme(<Banner {...bannerProps} />)
     // screen.logTestingPlaygroundURL()
-    expect(screen.getByRole('img')).toHaveAttribute('src', img)
+    expect(screen.getByRole('img')).toHaveAttribute('src', bannerProps.img)
     expect(
       screen.getByRole('heading', { name: /defy death/i })
     ).toBeInTheDocument()
     expect(screen.getByText(/play the new season/i)).toBeInTheDocument()
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
