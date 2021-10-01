3bed0b8879642f7bd243c373510fae326f4e7806
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-01 08:12:22 -0300

    test: create test to render Banner

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -1,13 +1,29 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Banner from '.'
 
 describe('<Banner />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Banner />)
-
-    expect(screen.getByRole('heading', { name: /Banner/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render the banner', () => {
+    const img = 'https://source.unsplash.com/user/willianjusten/1042x580'
+    const title = 'Defy death'
+    const subtitle = '<p>Play the new <strong>CrashLands</strong> season'
+    const buttonLabel = 'Buy now'
+    const buttonLink = '/games/defy-death'
+    renderWithTheme(
+      <Banner
+        img={img}
+        title={title}
+        subtitle={subtitle}
+        buttonLabel={buttonLabel}
+        buttonLink={buttonLink}
+      />
+    )
+    // screen.logTestingPlaygroundURL()
+    expect(screen.getByRole('img')).toHaveAttribute('src', img)
+    expect(
+      screen.getByRole('heading', { name: /defy death/i })
+    ).toBeInTheDocument()
+    expect(screen.getByText(/play the new season/i)).toBeInTheDocument()
   })
 })
