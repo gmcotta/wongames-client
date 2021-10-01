589919c83391cd5208b5bedfd7aee869f24d9001
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-01 19:55:41 -0300

    feat: create Ribbon detail

--- src/components/Ribbon/styles.ts
+++ src/components/Ribbon/styles.ts
@@ -5,14 +5,32 @@ import { RibbonColors, RibbonProps } from '.'
 const wrapperModifiers = {
   color: (theme: DefaultTheme, color: RibbonColors) => css`
     background-color: ${theme.colors[color]};
+    &::after {
+      color: ${theme.colors[color]};
+      filter: brightness(0.44);
+    }
   `,
   normal: (theme: DefaultTheme) => css`
+    right: -2rem;
     height: 3.6rem;
     font-size: ${theme.font.sizes.small};
+    padding: 0.6rem 4rem;
+    &::after {
+      top: 3.6rem;
+      border-top-width: 1rem;
+      border-right-width: 2rem;
+    }
   `,
   small: (theme: DefaultTheme) => css`
+    right: -1.5rem;
     height: 2.6rem;
     font-size: ${theme.font.sizes.xsmall};
+    padding: 0.4rem 2.8rem;
+    &::after {
+      top: 2.6rem;
+      border-top-width: 0.7rem;
+      border-right-width: 1.5rem;
+    }
   `
 }
 
@@ -20,5 +38,24 @@ export const Wrapper = styled.div<Omit<RibbonProps, 'children'>>`
   ${({ theme, color, size }) => css`
     ${!!color && wrapperModifiers.color(theme, color)}
     ${!!size && wrapperModifiers[size](theme)}
+    position: absolute;
+    top: ${theme.spacings.xsmall};
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: ${theme.colors.white};
+    font-weight: ${theme.font.bold};
   `}
+
+  &::after {
+    content: '';
+    position: absolute;
+    right: 0;
+    border-style: solid;
+    border-left-width: 0rem;
+    border-right-color: transparent;
+    border-bottom-color: transparent;
+    border-bottom-width: 1rem;
+  }
 `
