e05af97a47f8b6ba6106fd0f64342bf0db6f78a2
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-01 09:24:53 -0300

    test: create tests for Ribbon

--- src/components/Ribbon/test.tsx
+++ src/components/Ribbon/test.tsx
@@ -1,13 +1,41 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Ribbon from '.'
 
 describe('<Ribbon />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Ribbon />)
+  it('should render the Ribbon text', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+    expect(screen.getByText(/Best Seller/i)).toBeInTheDocument()
+  })
 
-    expect(screen.getByRole('heading', { name: /Ribbon/i })).toBeInTheDocument()
+  it('should render the Ribbon with primary color', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      backgroundColor: '#F231A5'
+    })
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render the Ribbon with secondary color', () => {
+    renderWithTheme(<Ribbon color="secondary">Best Seller</Ribbon>)
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      backgroundColor: '#3CD3C1'
+    })
+  })
+
+  it('should render the Ribbon with normal size', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      height: '3.6rem',
+      fontSize: '1.4rem'
+    })
+  })
+
+  it('should render the Ribbon with normal size', () => {
+    renderWithTheme(<Ribbon size="small">Best Seller</Ribbon>)
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
   })
 })
