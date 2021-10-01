8ad138685c126b4f416d8b3289016694cf9da0a5
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-01 08:11:53 -0300

    feat: work on Banner structure

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,8 +1,30 @@
+import Button from 'components/Button'
 import * as S from './styles'
 
-const Banner = () => (
+export type BannerProps = {
+  img: string
+  title: string
+  subtitle: string
+  buttonLabel: string
+  buttonLink: string
+}
+
+const Banner = ({
+  img,
+  title,
+  subtitle,
+  buttonLabel,
+  buttonLink
+}: BannerProps) => (
   <S.Wrapper>
-    <h1>Banner</h1>
+    <S.Image role="img" aria-label={title} src={img} />
+    <S.Caption>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle dangerouslySetInnerHTML={{ __html: subtitle }} />
+      <Button as="a" href={buttonLink} size="large">
+        {buttonLabel}
+      </Button>
+    </S.Caption>
   </S.Wrapper>
 )
 
