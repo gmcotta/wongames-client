397d805d7a23b6486ebafa5eb139390b82b900fc
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-02 10:06:35 -0300

    feat: create Highlight structure and tests

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,9 +1,15 @@
 import { Story, Meta } from '@storybook/react'
-import Highlight from '.'
+import Highlight, { HighlightProps } from '.'
 
 export default {
   title: 'Highlight',
-  component: Highlight
+  component: Highlight,
+  args: {
+    title: 'Red Dead is back',
+    subtitle: "Come and see John's new adventures",
+    buttonLabel: 'Buy now',
+    buttonlink: '/rdr2'
+  }
 } as Meta
 
-export const Default: Story = () => <Highlight />
+export const Default: Story<HighlightProps> = (args) => <Highlight {...args} />
