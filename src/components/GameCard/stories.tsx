744bbfa77a435501381d559f788a917f4d9de3fa
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-03 09:23:01 -0300

    feat: create structure for Game Card

---
+++ src/components/GameCard/stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from '@storybook/react'
+import GameCard, { GameCardProps } from '.'
+
+export default {
+  title: 'GameCard',
+  component: GameCard,
+  args: {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/population-zero-img.png',
+    price: 'R$ 235,00'
+  }
+} as Meta
+
+export const Default: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
