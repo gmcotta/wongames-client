7bcfb53bd483419064b03cc0d103a8d66dbe4749
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-04 15:14:28 -0300

    test: add tests for CardGameSlider

--- src/components/CardGameSlider/test.tsx
+++ src/components/CardGameSlider/test.tsx
@@ -1,13 +1,60 @@
-import { render, screen } from '@testing-library/react'
+import 'match-media-fake'
+import { screen } from '@testing-library/react'
 
+import { renderWithTheme } from 'utils/tests/helpers'
 import CardGameSlider from '.'
 
-describe('<CardGameSlider />', () => {
-  it('should render the heading', () => {
-    render(<CardGameSlider />)
+const items = [
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x141',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x142',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x143',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x144',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  }
+]
 
-    expect(
-      screen.getByRole('heading', { name: /CardGameSlider/i })
-    ).toBeInTheDocument()
+describe('<CardGameSlider />', () => {
+  it('should render with 4 items', () => {
+    const { container } = renderWithTheme(<CardGameSlider items={items} />)
+    expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
+  })
+
+  it('should render white arrows', () => {
+    renderWithTheme(<CardGameSlider items={items} color="white" />)
+    expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
+    expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
   })
 })
