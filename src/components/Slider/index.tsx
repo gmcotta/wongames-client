67c15828a0d53ef64131a8068300c57a76246c31
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-04 08:12:53 -0300

    feat: create structure for generic Slider

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,8 +1,17 @@
+import { ReactNode } from 'react'
+import SlickSlider, { Settings } from 'react-slick'
 import * as S from './styles'
 
-const Slider = () => (
+export type SliderSettings = Settings
+
+export type SliderProps = {
+  children: ReactNode
+  settings: SliderSettings
+}
+
+const Slider = ({ children, settings }: SliderProps) => (
   <S.Wrapper>
-    <h1>Slider</h1>
+    <SlickSlider {...settings}>{children}</SlickSlider>
   </S.Wrapper>
 )
 
--- src/components/Slider/stories.tsx
+++ src/components/Slider/stories.tsx
@@ -1,4 +1,7 @@
 import { Story, Meta } from '@storybook/react'
+import { Settings } from 'react-slick'
+import styled from 'styled-components'
+
 import Slider from '.'
 
 export default {
@@ -6,4 +9,53 @@ export default {
   component: Slider
 } as Meta
 
-export const Default: Story = () => <Slider />
+const horizontalSettings: Settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+}
+
+const verticalSettings: Settings = {
+  vertical: true,
+  verticalSwiping: true,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+}
+
+const Slide = styled.div`
+  background-color: gray;
+  width: 30rem;
+  padding: 10rem 0;
+  border: 1px solid red;
+  color: white;
+  text-align: center;
+`
+
+export const Horizontal: Story = () => (
+  <Slider settings={horizontalSettings}>
+    <Slide>1</Slide>
+    <Slide>2</Slide>
+    <Slide>3</Slide>
+    <Slide>4</Slide>
+    <Slide>5</Slide>
+    <Slide>6</Slide>
+    <Slide>7</Slide>
+  </Slider>
+)
+
+export const Vertical: Story = () => (
+  <Slider settings={verticalSettings}>
+    <Slide>1</Slide>
+    <Slide>2</Slide>
+    <Slide>3</Slide>
+    <Slide>4</Slide>
+    <Slide>5</Slide>
+    <Slide>6</Slide>
+    <Slide>7</Slide>
+  </Slider>
+)
