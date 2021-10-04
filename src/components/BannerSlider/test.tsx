4c70fca7411138d29480ddb5df7181d712a1466f
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-04 08:44:54 -0300

    test: add test for BannerSlider

--- src/components/BannerSlider/test.tsx
+++ src/components/BannerSlider/test.tsx
@@ -1,13 +1,48 @@
-import { render, screen } from '@testing-library/react'
+import 'match-media-fake'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import BannerSlider from '.'
 
-describe('<BannerSlider />', () => {
-  it('should render the heading', () => {
-    render(<BannerSlider />)
+const items = [
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbonText: 'Bestselling'
+  },
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x582',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+]
 
+describe('<BannerSlider />', () => {
+  it('should render the banner slider on vertical', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+    expect(container.querySelector('.slick-vertical')).toBeInTheDocument()
+  })
+
+  it('should render with 1 active item', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+    expect(container.querySelectorAll('.slick-slide')).toHaveLength(2)
+    expect(container.querySelectorAll('li.slick-active')).toHaveLength(1)
     expect(
-      screen.getByRole('heading', { name: /BannerSlider/i })
+      screen.getByRole('heading', { name: /defy death 1/i, hidden: false })
     ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /defy death 2/i, hidden: true })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the banner dots', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+    expect(container.querySelector('.slick-dots')).toBeInTheDocument()
+    expect(container.querySelectorAll('.slick-dots>li')).toHaveLength(2)
   })
 })
