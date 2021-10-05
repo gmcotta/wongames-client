f0083e37e33dc089fc056b4a5137a787b75dbeb6
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-05 09:14:20 -0300

    refactor: create mock files for components

---
+++ src/components/Highlight/mock.ts
@@ -0,0 +1,11 @@
+import { HighlightProps } from '.'
+
+export default {
+  title: 'Red Dead is back',
+  subtitle: "Come and see John's new adventures",
+  buttonLabel: 'Buy now',
+  buttonLink: '/rdr2',
+  backgroundImage: '/img/red-dead-img.jpeg',
+  floatImage: '',
+  contentAlignment: 'right'
+} as HighlightProps
--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,18 +1,11 @@
 import { Story, Meta } from '@storybook/react'
 import Highlight, { HighlightProps } from '.'
+import item from './mock'
 
 export default {
   title: 'Highlight',
   component: Highlight,
-  args: {
-    title: 'Red Dead is back',
-    subtitle: "Come and see John's new adventures",
-    buttonLabel: 'Buy now',
-    buttonLink: '/rdr2',
-    backgroundImage: '/img/red-dead-img.jpeg',
-    floatImage: '',
-    contentAlignment: 'right'
-  }
+  args: { ...item }
 } as Meta
 
 export const Default: Story<HighlightProps> = (args) => (
