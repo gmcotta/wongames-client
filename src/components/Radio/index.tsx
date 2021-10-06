072646a9f49b5adaf8a0a3755016c427f218b4d3
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-06 15:53:28 -0300

    feat: add onCheck and tests

--- src/components/Radio/index.tsx
+++ src/components/Radio/index.tsx
@@ -4,21 +4,40 @@ import * as S from './styles'
 type RadioValue = string | ReadonlyArray<string> | number
 
 export type RadioProps = {
+  onCheck?: (value?: RadioValue) => void
   label?: string
   labelFor?: string
   labelColor?: 'white' | 'black'
   value?: RadioValue
 } & InputHTMLAttributes<HTMLInputElement>
 
-const Radio = ({ label, labelFor, labelColor = 'white' }: RadioProps) => (
-  <S.Wrapper>
-    <S.Input type="radio" id={labelFor} />
-    {!!label && (
-      <S.Label htmlFor={labelFor} labelColor={labelColor}>
-        {label}
-      </S.Label>
-    )}
-  </S.Wrapper>
-)
+const Radio = ({
+  label,
+  labelFor = '',
+  labelColor = 'white',
+  value,
+  onCheck,
+  ...props
+}: RadioProps) => {
+  const onChange = () => {
+    !!onCheck && onCheck(value)
+  }
+  return (
+    <S.Wrapper>
+      <S.Input
+        type="radio"
+        id={labelFor}
+        {...props}
+        value={value}
+        onChange={onChange}
+      />
+      {!!label && (
+        <S.Label htmlFor={labelFor} labelColor={labelColor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
 
 export default Radio
--- src/components/Radio/test.tsx
+++ src/components/Radio/test.tsx
@@ -1,4 +1,5 @@
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Radio from '.'
@@ -21,5 +22,23 @@ describe('<Radio />', () => {
   it('should render without label', () => {
     renderWithTheme(<Radio />)
     expect(screen.queryByLabelText('Radio')).not.toBeInTheDocument()
+  })
+
+  it('should dispatch onCheck when label status changes', async () => {
+    const onCheck = jest.fn()
+    renderWithTheme(
+      <Radio
+        label="Radio"
+        labelFor="Radio"
+        onCheck={onCheck}
+        value="anyValue"
+      />
+    )
+    expect(onCheck).not.toHaveBeenCalled()
+    userEvent.click(screen.getByLabelText('Radio'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
+    })
+    expect(onCheck).toHaveBeenCalledWith('anyValue')
   })
 })
