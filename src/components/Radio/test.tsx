621edb10e6184809ca2715a2a872c71302068cef
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-06 15:43:08 -0300

    test: render Radio without label

--- src/components/Radio/test.tsx
+++ src/components/Radio/test.tsx
@@ -13,9 +13,13 @@ describe('<Radio />', () => {
 
   it('should render with label (black)', () => {
     renderWithTheme(<Radio label="Radio" labelColor="black" />)
-
     const label = screen.getByText('Radio')
     expect(label).toBeInTheDocument()
     expect(label).toHaveStyle({ color: '#030517' })
+  })
+
+  it('should render without label', () => {
+    renderWithTheme(<Radio />)
+    expect(screen.queryByLabelText('Radio')).not.toBeInTheDocument()
   })
 })
