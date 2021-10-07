530714fc71305000f6dcb1d3b6578f601192a2cb
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-07 08:17:42 -0300

    feat: add styles for Radio

--- src/components/Radio/stories.tsx
+++ src/components/Radio/stories.tsx
@@ -1,19 +1,55 @@
-import { Story, Meta } from '@storybook/react'
+import { Story, Meta } from '@storybook/react/types-6-0'
 import Radio, { RadioProps } from '.'
 
 export default {
   title: 'Radio',
   component: Radio,
-  args: {
-    label: 'Label',
-    labelFor: 'check',
-    labelColor: 'white'
-  },
   parameters: {
+    layout: 'fullscreen',
     backgrounds: {
       default: 'won-dark'
     }
+  },
+  args: {
+    labelColor: 'white'
+  },
+  argTypes: {
+    onCheck: { action: 'checked' }
   }
 } as Meta
 
-export const Default: Story<RadioProps> = (args) => <Radio {...args} />
+export const Default: Story<RadioProps> = (args) => (
+  <>
+    <div style={{ padding: 10 }}>
+      <Radio
+        label="primeiro"
+        labelFor="primeiro"
+        id="primeiro"
+        name="nome"
+        value="primeiro"
+        defaultChecked
+        {...args}
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Radio
+        label="segundo"
+        labelFor="segundo"
+        id="segundo"
+        name="nome"
+        value="segundo"
+        {...args}
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Radio
+        label="terceiro"
+        labelFor="terceiro"
+        id="terceiro"
+        name="nome"
+        value="terceiro"
+        {...args}
+      />
+    </div>
+  </>
+)
