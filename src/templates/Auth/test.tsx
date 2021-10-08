fb218eb17afa21ed17ccb625a54d9d2f480f49bd
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-08 08:25:00 -0300

    test: render Auth

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -1,10 +1,35 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Auth from '.'
 
 describe('<Auth />', () => {
-  it('should render the heading', () => {
-    render(<Auth />)
-    expect(screen.getByRole('heading', { name: /Auth/i })).toBeInTheDocument()
+  it('should render the auth template', () => {
+    renderWithTheme(
+      <Auth title="Auth template">
+        <form>
+          <input type="text" />
+        </form>
+      </Auth>
+    )
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+    expect(
+      screen.getByRole('heading', {
+        name: /All your favorite games in one place/i
+      })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', {
+        name: /won is the best and most complete gaming platform/i
+      })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /auth template/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByText(/Won Games 2020 © Todos os Direitos Reservados/i)
+    ).toBeInTheDocument()
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
+    screen.logTestingPlaygroundURL()
   })
 })
