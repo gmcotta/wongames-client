7a241bb6a295838f93bf353f723ed01aafe24373
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-08 08:24:08 -0300

    feat: create initial structure for Auth

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -1,8 +1,33 @@
+import { ReactNode } from 'react'
+
+import Heading from 'components/Heading'
+import Logo from 'components/Logo'
 import * as S from './styles'
 
-const Auth = () => (
+export type AuthProps = {
+  title: string
+  children: ReactNode
+}
+
+const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
-    <h1>Auth</h1>
+    <S.BannerSection>
+      <Logo />
+      <Heading>All your favorite games in one place</Heading>
+      <S.BannerSubtitle>
+        <strong>WON</strong> is the best and most complete gaming platform
+      </S.BannerSubtitle>
+      <S.BannerFooter>
+        Won Games 2020 © Todos os Direitos Reservados
+      </S.BannerFooter>
+    </S.BannerSection>
+    <S.ContentSection>
+      <Logo color="black" size="large" />
+      <Heading color="black" lineLeft lineColor="secondary">
+        {title}
+      </Heading>
+      {children}
+    </S.ContentSection>
   </S.Wrapper>
 )
 
--- src/templates/Auth/styles.ts
+++ src/templates/Auth/styles.ts
@@ -1,3 +1,11 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.main``
+
+export const BannerSection = styled.section``
+
+export const BannerSubtitle = styled.h3``
+
+export const BannerFooter = styled.footer``
+
+export const ContentSection = styled.section``
