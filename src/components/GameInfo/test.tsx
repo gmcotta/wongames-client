3455c4ade0560e4f79f9657214e802e0d0326ba6
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-11 08:14:30 -0300

    test: create tests to render Game Info

--- src/components/GameInfo/test.tsx
+++ src/components/GameInfo/test.tsx
@@ -3,11 +3,28 @@ import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameInfo from '.'
 
+const props = {
+  title: 'Borderland 3',
+  description: 'game description',
+  price: '210.00'
+}
+
 describe('<GameInfo />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<GameInfo />)
+  it('should render the game info', () => {
+    renderWithTheme(<GameInfo {...props} />)
     expect(
-      screen.getByRole('heading', { name: /GameInfo/i })
+      screen.getByRole('heading', { name: /borderland 3/i })
+    ).toBeInTheDocument()
+    expect(screen.getByText(/game description/i)).toBeInTheDocument()
+    expect(screen.getByText(/210.00/i)).toBeInTheDocument()
+  })
+  it('should render buttons', () => {
+    renderWithTheme(<GameInfo {...props} />)
+    expect(
+      screen.getByRole('button', { name: /add to cart/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /wish list/i })
     ).toBeInTheDocument()
   })
 })
