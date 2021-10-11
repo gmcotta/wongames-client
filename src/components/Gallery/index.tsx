067fb0f44549ba313e897eb79801fb996f51295a
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-11 15:56:11 -0300

    feat: close modal when esc key is pressed

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { ArrowBackIos as ArrowLeft } from '@styled-icons/material-outlined'
 import { ArrowForwardIos as ArrowRight } from '@styled-icons/material-outlined'
 import { Close } from '@styled-icons/material-outlined'
@@ -44,6 +44,15 @@ export type GalleryProps = {
 
 const Gallery = ({ items }: GalleryProps) => {
   const [isOpen, setIsOpen] = useState(false)
+  useEffect(() => {
+    const handleKeyUp = ({ key }: KeyboardEvent) => {
+      key === 'Escape' && setIsOpen(false)
+    }
+    window.addEventListener('keyup', handleKeyUp)
+    return () => {
+      window.removeEventListener('keyup', handleKeyUp)
+    }
+  }, [])
   return (
     <S.Wrapper>
       <Slider settings={settings}>
--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -34,9 +34,18 @@ describe('<Gallery />', () => {
     fireEvent.click(
       screen.getByRole('button', { name: /thumbnail - gallery image 1/i })
     )
-    expect(modal.getAttribute('aria-hidden')).toBe('false')
-    expect(modal).toHaveStyle({ opacity: 1 })
     fireEvent.click(screen.getByRole('button', { name: /close modal/i }))
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0, pointerEvents: 'none' })
+  })
+
+  it('should close modal if esc key was pressed', () => {
+    const { container } = renderWithTheme(<Gallery items={mock.slice(0, 2)} />)
+    const modal = screen.getByLabelText('modal')
+    fireEvent.click(
+      screen.getByRole('button', { name: /thumbnail - gallery image 1/i })
+    )
+    fireEvent.keyUp(container, { key: 'Escape' })
     expect(modal.getAttribute('aria-hidden')).toBe('true')
     expect(modal).toHaveStyle({ opacity: 0, pointerEvents: 'none' })
   })
