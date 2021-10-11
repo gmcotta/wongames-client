8c01a04a5196b9559c4804fb5fcf5ba4a23e5fa9
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-11 15:05:53 -0300

    feat: create thumbnail slider for Gallery

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,8 +1,57 @@
+import { ArrowBackIos as ArrowLeft } from '@styled-icons/material-outlined'
+import { ArrowForwardIos as ArrowRight } from '@styled-icons/material-outlined'
+
+import Slider, { SliderSettings } from 'components/Slider'
 import * as S from './styles'
 
-const Gallery = () => (
+const settings: SliderSettings = {
+  arrows: true,
+  slidesToShow: 4,
+  infinite: false,
+  lazyLoad: 'ondemand',
+  nextArrow: <ArrowRight aria-label="next image" />,
+  prevArrow: <ArrowLeft aria-label="previous image" />,
+  responsive: [
+    {
+      breakpoint: 1375,
+      settings: {
+        arrows: false,
+        slidesToShow: 3.2,
+        draggable: true
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2,
+        draggable: true
+      }
+    }
+  ]
+}
+
+type GalleryImageProps = {
+  src: string
+  label: string
+}
+
+export type GalleryProps = {
+  items: GalleryImageProps[]
+}
+
+const Gallery = ({ items }: GalleryProps) => (
   <S.Wrapper>
-    <h1>Gallery</h1>
+    <Slider settings={settings}>
+      {items.map((item) => (
+        <img
+          role="button"
+          key={item.src}
+          src={item.src}
+          alt={`Thumbnail - ${item.label}`}
+        />
+      ))}
+    </Slider>
   </S.Wrapper>
 )
 
--- src/components/Gallery/stories.tsx
+++ src/components/Gallery/stories.tsx
@@ -1,9 +1,24 @@
 import { Story, Meta } from '@storybook/react'
-import Gallery from '.'
+import Gallery, { GalleryProps } from '.'
+
+import mock from './mock'
 
 export default {
   title: 'Gallery',
-  component: Gallery
+  component: Gallery,
+  args: {
+    items: mock
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    },
+    layout: 'fullscreen'
+  }
 } as Meta
 
-export const Default: Story = () => <Gallery />
+export const Default: Story<GalleryProps> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <Gallery {...args} />
+  </div>
+)
