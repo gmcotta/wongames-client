7aef916480d89f54d12a476f5b987fe41dec2a38
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-15 11:46:27 -0300

    feat: create /profiles/orders page

--- src/pages/profile/orders.tsx
+++ src/pages/profile/orders.tsx
@@ -1,5 +1,20 @@
 import Profile from 'templates/Profile'
 
-export default function Orders() {
-  return <Profile>Oi</Profile>
+import ordersMock from 'components/OrdersList/mock'
+import OrdersList, { OrdersListProps } from 'components/OrdersList'
+
+export default function Orders(props: OrdersListProps) {
+  return (
+    <Profile>
+      <OrdersList {...props} />
+    </Profile>
+  )
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      items: ordersMock
+    }
+  }
 }
