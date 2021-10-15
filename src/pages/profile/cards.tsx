b9acfa8f7e791a984e0876c2a4bff6dec4d3ef4e
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-15 09:40:15 -0300

    feat: create /profile/cards structure

--- src/pages/profile/cards.tsx
+++ src/pages/profile/cards.tsx
@@ -1,5 +1,20 @@
+import CardsList, { CardsListProps } from 'components/CardsList'
 import Profile from 'templates/Profile'
 
-export default function Cards() {
-  return <Profile>Oi</Profile>
+import cardsMock from 'components/PaymentOptions/mock'
+
+export default function Cards(props: CardsListProps) {
+  return (
+    <Profile>
+      <CardsList {...props} />
+    </Profile>
+  )
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      cards: cardsMock
+    }
+  }
 }
