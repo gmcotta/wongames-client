b63b2a689ba4b6debbbd36459422b286fac7dd74
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-16 13:15:49 -0300

    feat: create ExploreSidebar hardcoded structure

--- src/components/ExploreSidebar/test.tsx
+++ src/components/ExploreSidebar/test.tsx
@@ -4,11 +4,34 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import ExploreSidebar from '.'
 
 describe('<ExploreSidebar />', () => {
-  it('should render the heading', () => {
+  it('should render the headings', () => {
     renderWithTheme(<ExploreSidebar />)
-
+    expect(screen.getByRole('heading', { name: /price/i })).toBeInTheDocument()
     expect(
-      screen.getByRole('heading', { name: /ExploreSidebar/i })
+      screen.getByRole('heading', { name: /sort by/i })
     ).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /system/i })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /genre/i })).toBeInTheDocument()
+  })
+
+  it('should render the inputs', () => {
+    renderWithTheme(<ExploreSidebar />)
+    expect(
+      screen.getByRole('checkbox', { name: /under \$50/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('radio', { name: /low to high/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('checkbox', { name: /windows/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('checkbox', { name: /action/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the filter button', () => {
+    renderWithTheme(<ExploreSidebar />)
+    expect(screen.getByRole('button', { name: /filter/i })).toBeInTheDocument()
   })
 })
