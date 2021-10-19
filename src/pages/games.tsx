89d4fd623262dc11118476217059722d9ce8419e
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-19 08:16:37 -0300

    feat: populate Explore page via ssg

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,16 +1,55 @@
 import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
-import gamesMock from 'components/CardGameSlider/mock'
 import sidebarMock from 'components/ExploreSidebar/mock'
+import { initializeApollo } from 'utils/apollo'
+import { gql } from '@apollo/client'
 
 export default function Game(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+  const { data } = await apolloClient.query({
+    query: gql`
+      query getGames {
+        games {
+          name
+          slug
+          cover {
+            url
+          }
+          developers {
+            name
+          }
+          price
+        }
+      }
+    `
+  })
+  type GameProps = {
+    name: string
+    slug: string
+    cover: {
+      url: string
+    }
+    developers: {
+      name: string
+    }[]
+    price: number
+  }
   return {
     props: {
-      games: gamesMock,
+      games: data.games.map((game: GameProps) => ({
+        title: game.name,
+        developer: game.developers[0].name,
+        img: `http://localhost:1337${game.cover.url}`,
+        price: new Intl.NumberFormat('en', {
+          style: 'currency',
+          currency: 'USD'
+        }).format(game.price)
+      })),
       filterItems: sidebarMock
-    }
+    },
+    revalidate: 60
   }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,15 @@
-import { gql } from '@apollo/client'
-
 import Home, { HomeTemplateProps } from 'templates/Home'
 import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/CardGameSlider/mock'
 import highlightMock from 'components/Highlight/mock'
-import { initializeApollo } from 'utils/apollo'
-
-const GET_GAMES = gql`
-  query getGames {
-    games {
-      name
-    }
-  }
-`
 
 export default function Index(props: HomeTemplateProps) {
-  if (props.data) return <pre>{JSON.stringify(props.data, null, 2)}</pre>
   return <Home {...props} />
 }
 
 export async function getStaticProps() {
-  const apolloClient = initializeApollo()
-  const { data } = await apolloClient.query({ query: GET_GAMES })
   return {
     props: {
-      data,
-      initialApolloState: apolloClient.cache.extract(),
       banners: bannersMock,
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
