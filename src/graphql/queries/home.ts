96dacda225651a5e74769979116801c6308084a7
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-22 10:33:08 -0300

    refactor: use today date to get home data

--- src/graphql/queries/home.ts
+++ src/graphql/queries/home.ts
@@ -3,19 +3,19 @@ import { BannerFragment } from 'graphql/fragment/banner'
 import { GameFragment, HighlightFragment } from 'graphql/fragment/game'
 
 export const QUERY_HOME = gql`
-  query QueryHome {
+  query QueryHome($date: Date!) {
     banners {
       ...BannerFragment
     }
     newGames: games(
-      where: { release_date_lte: "2021-10-01" }
+      where: { release_date_lte: $date }
       sort: "release_date:desc"
       limit: 8
     ) {
       ...GameFragment
     }
     upcomingGames: games(
-      where: { release_date_gt: "2021-10-01" }
+      where: { release_date_gt: $date }
       sort: "release_date:asc"
       limit: 8
     ) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { GetStaticProps } from 'next'
 import Home, { HomeTemplateProps } from 'templates/Home'
 import { initializeApollo } from 'utils/apollo'
 import { QUERY_HOME } from 'graphql/queries/home'
-import { QueryHome } from 'graphql/generated/QueryHome'
+import { QueryHome, QueryHomeVariables } from 'graphql/generated/QueryHome'
 import { bannerMapper, gamesMapper, highlightMapper } from 'utils/mappers'
 
 export default function Index(props: HomeTemplateProps) {
@@ -11,10 +11,14 @@ export default function Index(props: HomeTemplateProps) {
 
 export const getStaticProps: GetStaticProps = async () => {
   const apolloClient = initializeApollo()
+  const TODAY_DATE = new Date().toISOString().slice(0, 10)
   const {
     data: { banners, newGames, upcomingGames, freeGames, sections }
-  } = await apolloClient.query<QueryHome>({
-    query: QUERY_HOME
+  } = await apolloClient.query<QueryHome, QueryHomeVariables>({
+    query: QUERY_HOME,
+    variables: {
+      date: TODAY_DATE
+    }
   })
   const popularGames = sections?.popularGames?.games
   const popularHighlight = sections?.popularGames?.highlight
