2b147161b3524389a6d9d243f21c1fbd03ac5b0d
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-28 22:17:14 -0300

    feat: create local storage util

---
+++ src/utils/localStorage/test.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-global-assign */
+import { getStorageItem, setStorageItem } from '.'
+
+describe('getStorageItem', () => {
+  beforeEach(() => {
+    window.localStorage.clear()
+  })
+  it('should return the item from local storage', () => {
+    window.localStorage.setItem(
+      'WONGAMES_cartItems',
+      JSON.stringify(['1', '2'])
+    )
+    expect(getStorageItem('cartItems')).toStrictEqual(['1', '2'])
+  })
+})
+
+describe('setStorageItem', () => {
+  beforeEach(() => {
+    window.localStorage.clear()
+  })
+  it('should add the item on local storage', () => {
+    setStorageItem('cartItems', ['1', '2'])
+    expect(window.localStorage.getItem('WONGAMES_cartItems')).toStrictEqual(
+      JSON.stringify(['1', '2'])
+    )
+  })
+})
