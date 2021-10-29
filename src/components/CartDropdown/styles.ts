66c48d005f437725a07aa4ef23800f5e6154785b
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-28 21:39:58 -0300

    feat: add overlay on dropdown for closing component

--- src/components/CartDropdown/styles.ts
+++ src/components/CartDropdown/styles.ts
@@ -5,5 +5,6 @@ import * as CartListStyles from 'components/CartList/styles'
 export const Wrapper = styled.div`
   ${CartListStyles.Wrapper} {
     width: 56rem;
+    cursor: default;
   }
 `
--- src/components/Dropdown/test.tsx
+++ src/components/Dropdown/test.tsx
@@ -33,4 +33,21 @@ describe('<Dropdown />', () => {
       'pointer-events': 'none'
     })
   })
+  it('should toggle the content when closing on overlay', () => {
+    renderWithTheme(<Dropdown title={'Title'}>Content</Dropdown>)
+    const title = screen.getByText(/title/i)
+    const content = screen.getByText(/content/i)
+    const overlay = content.nextElementSibling
+    userEvent.click(title)
+    expect(overlay).toHaveAttribute('aria-hidden', 'false')
+    expect(overlay).toHaveStyle({
+      opacity: 1
+    })
+    userEvent.click(overlay!)
+    expect(overlay).toHaveAttribute('aria-hidden', 'true')
+    expect(overlay).toHaveStyle({
+      opacity: 0,
+      'pointer-events': 'none'
+    })
+  })
 })
