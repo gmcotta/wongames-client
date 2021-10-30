f6760cd940a39bfebf9ef6a2a1bfbe8ec8d1f4ff
Gustavo Cotta <gmcotta34@gmail.com>
2021-10-30 17:58:42 -0300

    feat: add id on gameInfo prop

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -80,6 +80,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const props = {
     coverSrc: `http://localhost:1337${game.cover?.src}`,
     gameInfo: {
+      id: game.id,
       title: game.name,
       description: game.short_description,
       price: game.price
