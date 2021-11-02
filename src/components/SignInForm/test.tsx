fb11c29c1285433d4e76f013a7aeadb8d056d9e3
Gustavo Cotta <gmcotta34@gmail.com>
2021-11-02 17:01:55 -0300

    test: add test to SignInForm

--- src/components/SignInForm/test.tsx
+++ src/components/SignInForm/test.tsx
@@ -1,17 +1,24 @@
-import { screen, render } from 'utils/testUtils'
+import userEvent from '@testing-library/user-event'
+import { screen, render, waitFor } from 'utils/testUtils'
 
 import SignInForm from '.'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const useRouter = jest.spyOn(require('next/router'), 'useRouter')
 const push = jest.fn()
-
+const prefetch = jest.fn(() => Promise.resolve(true))
 useRouter.mockImplementation(() => ({
   push,
+  prefetch,
   query: '',
   asPath: '',
   route: '/'
 }))
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const signIn = jest.spyOn(require('next-auth/client'), 'signIn')
+let result = {}
+signIn.mockImplementation(() => result)
 
 describe('<SignInForm />', () => {
   it('should render the form', () => {
@@ -23,19 +30,55 @@ describe('<SignInForm />', () => {
         name: /sign in now/i
       })
     ).toBeInTheDocument()
-    expect(container.firstChild).toMatchSnapshot()
-  })
-
-  it('should render the forgot password link', () => {
-    render(<SignInForm />)
     expect(
       screen.getByRole('link', { name: /forgot your password\?/i })
     ).toBeInTheDocument()
-  })
-
-  it('should render the text and link for sign up', () => {
-    render(<SignInForm />)
     expect(screen.getByText(/Don't have an account\?/i)).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument()
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should validate form', async () => {
+    render(<SignInForm />)
+    userEvent.click(
+      screen.getByRole('button', {
+        name: /sign in now/i
+      })
+    )
+    expect(
+      await screen.findAllByText(/is not allowed to be empty/i)
+    ).toHaveLength(2)
+  })
+
+  it('should sign in properly', async () => {
+    result = {
+      url: '/test'
+    }
+    render(<SignInForm />)
+    userEvent.type(screen.getByPlaceholderText(/email/i), 'test@email.com')
+    userEvent.type(screen.getByPlaceholderText(/password/i), '123')
+    userEvent.click(
+      screen.getByRole('button', {
+        name: /sign in now/i
+      })
+    )
+    await waitFor(() => {
+      expect(push).toHaveBeenCalledWith('/test')
+    })
+  })
+
+  it('should return sign in error message', async () => {
+    result = {
+      url: null
+    }
+    render(<SignInForm />)
+    userEvent.type(screen.getByPlaceholderText(/email/i), 'error@email.com')
+    userEvent.type(screen.getByPlaceholderText(/password/i), '123')
+    userEvent.click(
+      screen.getByRole('button', {
+        name: /sign in now/i
+      })
+    )
+    expect(await screen.findByText(/username or password is invalid/i))
   })
 })
