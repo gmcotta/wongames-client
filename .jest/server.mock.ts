54fa63864683aa2c6297cbfdda34ca1dd2d0c95a
Gustavo Cotta <gmcotta34@gmail.com>
2021-11-02 14:30:02 -0300

    chore: config node-fetch

--- .jest/server.mock.ts
+++ .jest/server.mock.ts
@@ -1,7 +1,7 @@
+global.fetch = require('node-fetch')
+
 import { server } from '../src/utils/mockServer/server'
 
 beforeAll(() => server.listen())
-
 afterEach(() => server.resetHandlers())
-
 afterAll(() => server.close())
