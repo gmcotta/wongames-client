91c419406e2cdcbc60de69a785d13defe7de2405
Gustavo Cotta <gmcotta34@gmail.com>
2021-11-03 22:07:52 -0300

    feat: create useWishlist mock

---
+++ src/hooks/use-wishlist/mock.ts
@@ -0,0 +1,64 @@
+import { QUERY_WISHLIST } from 'graphql/queries/wishlist'
+
+const gameMock = (id: string) => ({
+  id,
+  name: `Game ${id}`,
+  slug: `slug-game-${id}`,
+  price: 30.3,
+  developers: [{ name: 'developer' }],
+  cover: {
+    src: `/game${id}.jpg`
+  },
+  __typename: 'Game'
+})
+
+export const wishlistMock = {
+  request: {
+    query: QUERY_WISHLIST,
+    variables: {
+      identifier: 'email@email.com'
+    },
+    context: {
+      session: {
+        jwt: 'token'
+      }
+    }
+  },
+  result: {
+    data: {
+      wishlists: [
+        {
+          id: '1',
+          games: [gameMock('1'), gameMock('2'), gameMock('3')]
+        }
+      ]
+    }
+  }
+}
+
+export const wishlistItemsMock = [
+  {
+    id: '1',
+    title: `Game 1`,
+    slug: `slug-game-1`,
+    price: 30.3,
+    developer: 'developer',
+    img: 'http://localhost:1337/game1.jpg'
+  },
+  {
+    id: '2',
+    title: `Game 2`,
+    slug: `slug-game-2`,
+    price: 30.3,
+    developer: 'developer',
+    img: 'http://localhost:1337/game2.jpg'
+  },
+  {
+    id: '3',
+    title: `Game 3`,
+    slug: `slug-game-3`,
+    price: 30.3,
+    developer: 'developer',
+    img: 'http://localhost:1337/game3.jpg'
+  }
+]
