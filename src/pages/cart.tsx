5de773c972854b389f2b61423555dac444b3f1ab
Gustavo Cotta <gmcotta34@gmail.com>
2021-11-06 11:59:02 -0300

    feat: make cart as protected route

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,22 +1,30 @@
-import CartTemplate, { CartTemplateProps } from 'templates/Cart'
-import cartListMock from 'components/CartList/mock'
-import cardsMock from 'components/PaymentOptions/mock'
+import { GetServerSidePropsContext } from 'next'
+
 import { initializeApollo } from 'utils/apollo'
+import protectedRoutes from 'utils/protectedRoutes'
+import { gamesMapper, highlightMapper } from 'utils/mappers'
+
 import { QueryRecommended } from 'graphql/generated/QueryRecommended'
 import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
-import { gamesMapper, highlightMapper } from 'utils/mappers'
+
+import CartTemplate, { CartTemplateProps } from 'templates/Cart'
+
+import cartListMock from 'components/CartList/mock'
+import cardsMock from 'components/PaymentOptions/mock'
 
 export default function Cart(props: CartTemplateProps) {
   return <CartTemplate {...props} />
 }
 
-export async function getServerSideProps() {
-  const apolloClient = initializeApollo()
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await protectedRoutes(context)
+  const apolloClient = initializeApollo(null, session)
   const { data } = await apolloClient.query<QueryRecommended>({
     query: QUERY_RECOMMENDED
   })
   return {
     props: {
+      session,
       recommendedTitle: data.recommended?.section?.title,
       recommendedGames: gamesMapper(data.recommended?.section?.games),
       recommendedHighlight: highlightMapper(
--- src/utils/protectedRoutes.ts
+++ src/utils/protectedRoutes.ts
@@ -4,9 +4,11 @@ import { getSession } from 'next-auth/client'
 async function protectedRoutes(context: GetServerSidePropsContext) {
   const session = await getSession(context)
   if (!session) {
-    context.res.writeHead(302, {
-      location: `/sign-in?callbackUrl=${context.resolvedUrl}`
-    })
+    context.res.setHeader(
+      'Location',
+      `/sign-in?callbackUrl=${context.resolvedUrl}`
+    )
+    context.res.statusCode = 302
     context.res.end()
   }
   return session
