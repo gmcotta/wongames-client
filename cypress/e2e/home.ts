1c6b1fc63dcd902be4ec747165dead62771935c9
Gustavo Cotta <gmcotta34@gmail.com>
2021-11-11 21:32:49 -0300

    test: create custom command to check banner

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -4,14 +4,6 @@
 describe('Home Page', () => {
   it('should render home sections', () => {
     cy.visit('/')
-    cy.get('.slick-slider').within(() => {
-      cy.findByRole('heading', { name: /the elder scrolls iv: oblivion/i })
-      cy.findByRole('link', { name: /buy now/i })
-      cy.get('.slick-dots > :nth-child(2) > button').click()
-      // eslint-disable-next-line cypress/no-unnecessary-waiting
-      cy.wait(500)
-      cy.findByRole('heading', { name: /kenshi/i })
-      cy.findByRole('link', { name: /buy now/i })
-    })
+    cy.shouldRenderBanner()
   })
 })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -27,3 +27,15 @@
 import '@testing-library/cypress/add-commands'
 
 Cypress.Commands.add('google', () => cy.visit('https://google.com'))
+
+Cypress.Commands.add('shouldRenderBanner', () =>
+  cy.get('.slick-slider').within(() => {
+    cy.findByRole('heading', { name: /the elder scrolls iv: oblivion/i })
+    cy.findByRole('link', { name: /buy now/i })
+    cy.get('.slick-dots > :nth-child(2) > button').click()
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(500)
+    cy.findByRole('heading', { name: /kenshi/i })
+    cy.findByRole('link', { name: /buy now/i })
+  })
+)
