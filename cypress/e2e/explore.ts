52d38744179668090cd3727af6a9cd99e885a1d5
Gustavo Cotta <gmcotta34@gmail.com>
2021-11-13 21:19:46 -0300

    test(explore page): test filter by price

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -1,11 +1,23 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../support/index.d.ts" />
+
 import {
   genreFields,
   platformFields,
   sortFields,
   priceFields
 } from '../../src/utils/filter/fields'
+
+const selectFilterAndCheckIfFirstGameCardPriceIsLessThan = (price: number) => {
+  cy.findByText(`Under $${price}`).click()
+  cy.location('href').should('contain', `price_lte=${price}`)
+  cy.getByDataCy('game-card')
+    .first()
+    .within(() => {
+      cy.shouldPriceBeLessThan(price)
+    })
+}
+
 describe('Explore Page', () => {
   before(() => {
     cy.visit('/games')
@@ -28,7 +40,7 @@ describe('Explore Page', () => {
     cy.getByDataCy('game-card').should('have.length', 30)
   })
 
-  it.only('should sort by price', () => {
+  it('should sort by price', () => {
     cy.findByText(/Lowest to highest/i).click()
     cy.location('href').should('contain', 'sort=price%3Aasc')
     cy.getByDataCy('game-card')
@@ -43,5 +55,21 @@ describe('Explore Page', () => {
       .within(() => {
         cy.shouldPriceBeGreaterThan(0)
       })
+  })
+
+  it.only('should filter by price', () => {
+    cy.findByText(/Highest to lowest/i).click()
+    cy.findByText(/free/i).click()
+    cy.location('href').should('contain', 'price_lte=0')
+    cy.getByDataCy('game-card')
+      .first()
+      .within(() => {
+        cy.findByText(/free/i).should('exist')
+      })
+    selectFilterAndCheckIfFirstGameCardPriceIsLessThan(50)
+    selectFilterAndCheckIfFirstGameCardPriceIsLessThan(100)
+    selectFilterAndCheckIfFirstGameCardPriceIsLessThan(150)
+    selectFilterAndCheckIfFirstGameCardPriceIsLessThan(250)
+    selectFilterAndCheckIfFirstGameCardPriceIsLessThan(500)
   })
 })
