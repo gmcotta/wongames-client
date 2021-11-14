e692973072b5411fa2a1e9e5002686daddcc9eca
Gustavo Cotta <gmcotta34@gmail.com>
2021-11-13 21:47:05 -0300

    test(explore page): test filter by platforms and genres, render empty

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -57,7 +57,7 @@ describe('Explore Page', () => {
       })
   })
 
-  it.only('should filter by price', () => {
+  it('should filter by price', () => {
     cy.findByText(/Highest to lowest/i).click()
     cy.findByText(/free/i).click()
     cy.location('href').should('contain', 'price_lte=0')
@@ -71,5 +71,75 @@ describe('Explore Page', () => {
     selectFilterAndCheckIfFirstGameCardPriceIsLessThan(150)
     selectFilterAndCheckIfFirstGameCardPriceIsLessThan(250)
     selectFilterAndCheckIfFirstGameCardPriceIsLessThan(500)
+  })
+
+  it('should filter by platform', () => {
+    cy.findByText(/windows/i).click()
+    cy.location('href').should('contain', 'platforms=windows')
+    cy.findByText(/windows/i).click()
+    cy.findByText(/linux/i).click()
+    cy.location('href').should('contain', 'platforms=linux')
+    cy.findByText(/linux/i).click()
+    cy.findByText(/mac os/i).click()
+    cy.location('href').should('contain', 'platforms=mac')
+    cy.findByText(/mac os/i).click()
+  })
+
+  it('should filter by genre', () => {
+    cy.findByText(/action/i).click()
+    cy.location('href').should('contain', 'categories=action')
+    cy.findByText(/action/i).click()
+
+    cy.findByText(/adventure/i).click()
+    cy.location('href').should('contain', 'categories=adventure')
+    cy.findByText(/adventure/i).click()
+
+    cy.findByText(/sports/i).click()
+    cy.location('href').should('contain', 'categories=sports')
+    cy.findByText(/sports/i).click()
+
+    cy.findByText(/puzzle/i).click()
+    cy.location('href').should('contain', 'categories=puzzle')
+    cy.findByText(/puzzle/i).click()
+
+    cy.findByText(/horror/i).click()
+    cy.location('href').should('contain', 'categories=horror')
+    cy.findByText(/horror/i).click()
+
+    cy.findByText(/^platform$/i).click()
+    cy.location('href').should('contain', 'categories=platform')
+    cy.findByText(/^platform$/i).click()
+
+    cy.findByText(/fantasy/i).click()
+    cy.location('href').should('contain', 'categories=fantasy')
+    cy.findByText(/fantasy/i).click()
+
+    cy.findByText(/^rpg/i).click()
+    cy.location('href').should('contain', 'categories=role-playing')
+    cy.findByText(/^rpg/i).click()
+
+    cy.findByText(/^jrpg/i).click()
+    cy.location('href').should('contain', 'categories=jrpg')
+    cy.findByText(/^jrpg/i).click()
+
+    cy.findByText(/simulation/i).click()
+    cy.location('href').should('contain', 'categories=simulation')
+    cy.findByText(/simulation/i).click()
+
+    cy.findByText(/strategy/i).click()
+    cy.location('href').should('contain', 'categories=strategy')
+    cy.findByText(/strategy/i).click()
+
+    cy.findByText(/shooter/i).click()
+    cy.location('href').should('contain', 'categories=shooter')
+    cy.findByText(/shooter/i).click()
+  })
+
+  it.only('should return empty if no games were found', () => {
+    cy.findByText(/mac os/i).click()
+    cy.findByText(/sports/i).click()
+    cy.getByDataCy('game-card').should('not.exist')
+    cy.findByRole('heading', { name: /ooops/i }).should('exist')
+    cy.findByText(/we didn't find any games with this filter/i)
   })
 })
