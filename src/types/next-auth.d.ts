67fc575bb5c79ea2de257471d57d6320aac85ea0
Gustavo Cotta <gmcotta34@gmail.com>
2021-11-18 20:00:23 -0300

    refactor: test if build is working on pipeline

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,5 +4,11 @@ declare module 'next-auth' {
   interface Session extends DefaultSession {
     /** This is an example. You can find me in types/next-auth.d.ts */
     id?: string
+    user?: {
+      name?: string | null
+      email?: string | null
+      image?: string | null
+    }
+    expires?: string
   }
 }
